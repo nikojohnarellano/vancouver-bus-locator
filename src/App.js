b5435ff5f12ab2f96cb35d433eedbe8582e66673
Niko Arellano <narellano@my.bcit.ca>
2018-08-08 20:42:28 -0700

    Initial setup. Set map, stores, reducers

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactMapGL from 'react-map-gl';
+import ReactLoading from 'react-loading';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    loading : true,
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: null,
+      longitude: null,
+      zoom: 13
+    }
+  };
+
+  getCurrentPosition() {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+
+      this.setState({ 
+        viewport: {
+          ...this.state.viewport,
+          latitude,
+          longitude
+        }
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.getCurrentPosition();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+          <ReactMapGL
+            {...this.state.viewport}
+            mapStyle={'mapbox://styles/nikoootine/cjklykkie1j7h2royf7304dqa'}
+            mapboxApiAccessToken={"pk.eyJ1Ijoibmlrb29vdGluZSIsImEiOiJjamtsdnlpZXUyNXBiM3BvM243dWM0ZWRpIn0.MKmDh_tL-VSwb9qGBdJtlQ"}
+            onViewportChange={(viewport) => this.setState({viewport})}
+          />
+          <div style={{ position : 'absolute', left : 20, top : 20 }}>
+            <span>HEYHEYHEY</span>
+          </div>
+        </div>
     );
   }
 }
